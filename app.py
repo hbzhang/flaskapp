67b501a775f5c476af84c3a23280bcddf2b5ef47
hongbo zhang <hbzhang@vt.edu>
2020-04-13 09:57:12 -0400

    change

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('download.html')
+#@app.route('/')
+#def index():
+#    return render_template('download.html')
 
 
 
