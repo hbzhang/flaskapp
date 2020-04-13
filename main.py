95829ac0d1d4c88aedfcd9631ccc97a6c18e5ff3
hongbo zhang <hbzhang@vt.edu>
2020-04-13 10:38:04 -0400

    change

--- main.py
+++ main.py
@@ -9,6 +9,34 @@ from flask import Flask, Response, render_template
 def download():
 	return render_template('download.html')
 
+@app.route('/getemployee', methods=['GET'])
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'],
+                                      _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+@app.route('/getemployee', methods=['GET'])
+def get_employee():
+	conn = None
+	cursor = None
+	try:
+		conn = mysql.connect()
+		cursor = conn.cursor(pymysql.cursors.DictCursor)
+
+		cursor.execute("SELECT emp_id, emp_first_name, emp_last_name, emp_designation FROM employee")
+		result = cursor.fetchall()
+	    for row in result:
+		line = [str(row['emp_id']) + ',' + row['emp_first_name'] + ',' + row['emp_last_name'] + ',' + row[
+			'emp_designation']]
+	return jsonify({'tasks': [{"ee":"ff"}]})
+	#return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+
 @app.route('/download/report/csv')
 def download_report():
 	conn = None
@@ -16,13 +44,13 @@ def download_report():
 	try:
 		conn = mysql.connect()
 		cursor = conn.cursor(pymysql.cursors.DictCursor)
-		
+
 		cursor.execute("SELECT emp_id, emp_first_name, emp_last_name, emp_designation FROM employee")
 		result = cursor.fetchall()
 
 		output = io.StringIO()
 		writer = csv.writer(output)
-		
+
 		line = ['Emp Id, Emp First Name, Emp Last Name, Emp Designation']
 		writer.writerow(line)
 
@@ -31,12 +59,12 @@ def download_report():
 			writer.writerow(line)
 
 		output.seek(0)
-		
+
 		return Response(output, mimetype="text/csv", headers={"Content-Disposition":"attachment;filename=employee_report.csv"})
 	except Exception as e:
 		print(e)
 	finally:
-		cursor.close() 
+		cursor.close()
 		conn.close()
 
 if __name__ == "__main__":
